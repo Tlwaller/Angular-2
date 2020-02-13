55064a76bde78f8e43d8e9b398f6c36821791018
Trey Waller <Trey@treys-mbp.attlocal.net>
2020-02-13 16:53:27 -0600

    pipes are cool!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,9 @@ export class AppComponent {
   title = 'Change me!';
   public userInput : string = '';
 
+  date = new Date(2001, 2, 21);
+
+
   handleInput(e) {
     this.userInput = e.target.value;
     console.log(this.userInput);
