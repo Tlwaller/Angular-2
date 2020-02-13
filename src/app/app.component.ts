463479a9a9716e42b35550c43d749444b339e4bb
Trey Waller <Trey@treys-mbp.attlocal.net>
2020-02-13 16:42:25 -0600

    ngModel 2 way binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,11 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  title = 'learn-angular';
-  userInput = '';
+  title = 'Change me!';
+  public userInput : string = '';
 
   handleInput(e) {
     this.userInput = e.target.value;
     console.log(this.userInput);
+  }
+
+  buttonClick = () => {
+    this.title = this.userInput;
   }
 }
