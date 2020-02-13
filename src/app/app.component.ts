6b3da1f26041380ab800b46fd0d0a3bf5f0d1dfe
Trey Waller <Trey@treys-mbp.attlocal.net>
2020-02-13 15:51:16 -0600

    event listeners

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'learn-angular';
+  userInput = '';
+
+  handleInput(e) {
+    this.userInput = e.target.value;
+    console.log(this.userInput);
+  }
 }
